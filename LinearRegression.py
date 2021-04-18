f63e770eb65a5b887934948bc65335ecf3db729c
Mina Odabaş <61796541+minaodabas@users.noreply.github.com>
2021-04-18 20:30:10 +0300

    Add files via upload
    Naive Bayes and Linear Regression Algorithms

---
+++ LinearRegression.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+dataset = pd.read_csv('C:\\Users\\minao\\Desktop\\student_scores.csv')
+dataset.shape
+dataset.head()
+dataset.describe()
+
+dataset.plot(x='Hours', y='Scores', style='o')
+plt.title('Hours vs Percentage')
+plt.xlabel('Hours Studied')
+plt.ylabel('Percentage Score')
+plt.show()
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+print(regressor.intercept_)
+print(regressor.coef_)
+
+y_pred = regressor.predict(X_test)
+
+df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+df
+
+from sklearn import metrics
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
